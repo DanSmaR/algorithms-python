2771abc600c588466cc81e053ff912a80d315ed2
Danilo Martins <dansm86@outlook.com>
2023-03-23 23:52:22 -0300

    feat/req01-implemented challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    target_frequency = 0
+    for start_period, final_period in permanence_period:
+        if not isinstance(start_period, int) or not isinstance(
+            final_period, int
+        ):
+            return None
+        if start_period <= target_time <= final_period:
+            target_frequency += 1
+    return target_frequency
